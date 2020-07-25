52aa9d0142c038b396e0811ee0cd56b54a5284e8
Affan Dhia Ardhiva <affandhia@users.noreply.github.com>
2020-07-25 21:28:55 +0700

    update unintended changes

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,9 +1,9 @@
 <script>
-  export let value;
+  export let value
 </script>
 
-<input 
-type="text"
-  class="bg-white border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" 
-  bind:value={value}
->
+<input
+  type="text"
+  class="bg-white border border-gray-300 rounded-lg py-2 px-4 block w-full
+  appearance-none leading-normal"
+  bind:value />
