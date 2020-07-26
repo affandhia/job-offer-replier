a22a5b0e5d177a68940999f2fe965f9794945169
Affan Dhia Ardhiva <affandhia@users.noreply.github.com>
2020-07-26 13:17:18 +0700

    improve UI on form and text

---
+++ src/components/Radio.svelte
@@ -0,0 +1,24 @@
+<script>
+  import cx from "classnames";
+  export let value;
+  export let label = '';
+  export let isDisabled = false;
+  export let group;
+  let className = '';
+  export { className as class }
+  $: isActive = group === value;
+</script>
+
+<style>
+  .radio {
+    @apply text-xs transition-colors duration-300 flex-auto text-gray-500;
+  }
+  .active {
+    @apply text-gray-700;
+  }
+</style>
+
+<label class={cx('radio', className)} class:active={isActive}>
+  <input type="radio" bind:group disabled={isDisabled} value={value}>
+  {label}
+</label>
---
+++ src/components/RadioGroup.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Radio from "src/components/Radio.svelte";
+  export let value = undefined;
+  export let options = [];
+</script>
+
+{#each options as item}
+  <div class="flex flex-row items-center w-full">
+    <Radio bind:group={value} label={item.label} isDisabled={item.isDisabled} value={item.value} />
+  </div>
+{/each}
+<slot>
+</slot>
