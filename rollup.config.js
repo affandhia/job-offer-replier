00055e579ac7a877750e19703447a57e42ac0992
Affan Dhia Ardhiva <affandhia@users.noreply.github.com>
2020-07-30 05:25:13 +0700

    add icon and tidy up package json

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
-import url from '@rollup/plugin-url';
 import { terser } from 'rollup-plugin-terser';
 import livereload from 'rollup-plugin-livereload';
 import svelteSVG from 'rollup-plugin-svelte-svg';
